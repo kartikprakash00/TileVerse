d5ee6ec424213fa92479035bb26cea9b47056774
Kartik Prakash <kartikprakash@Kartiks-MacBook.local>
2024-08-22 04:36:19 +0530

    Menu dropdown added

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,19 @@ function startGame() {
     window.location.href = './choose.html';
 }
 
-// function menu() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
+function menu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-// window.onclick = function (event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
